f87f624d7ed2e92b8943e884b6e8972b9e26b38b
Mercury06 <zetmail@ukr.net>
2022-01-25 13:16:22 +0300

    routes file added and mongodb connected

--- server/index.js
+++ server/index.js
@@ -1,1 +1,39 @@
-console.log("server init")
+const express = require ("express")
+//const res = require("express/lib/response")
+const mongoose = require ("mongoose")
+//var config = require('config');
+const authRouter = require ("./routes/auth.routes")
+
+const app = express()
+//const PORT = config.get('serverPort')
+const PORT = 5000;
+//console.log("This data from config:", serverPort)
+console.log("This data from config:", PORT)
+
+//app.use("/api/auth", authRouter)
+
+const start = async () => {
+    try {
+        //await mongoose.connect(config.get("dbUrl"))
+        //await mongoose.connect(config.get("mongodb+srv://agregator:cloud_password93@cloud-disk.uvdnf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"))
+
+        app.listen (PORT, () => {
+            console.log('Server started on port ', PORT)
+        })
+    } catch (e) {
+        console.log(e)
+        //res.send({message: "Server error"})
+    }
+}
+
+start()
+// app.get("/", (req, res) => {  
+//     // res.write("Hello")
+//     // res.end()
+//     res.send("shows only First Line")
+//     res.send("won't show second Line")
+// }
+
+// )
+
+// app.listen(PORT, () => {console.log('Server started on port ', PORT)})
---
+++ server/routes/auth.routes.js
@@ -0,0 +1,37 @@
+const Router = require ("express");
+const User = require ("../models/User")
+const bcrypt = require ("bcryptjs") 
+const {check, validationResult} =  require ('express-validator')
+
+const router = new Router()  //создаем объект
+
+router.post('/registration', 
+    [
+        check('email', 'Uncorrect email').isEmail(),
+        check('password', 'Password must be longer than 3 and shorter than 12').isLength({min:3, max:12})
+    ],    
+    async (req, res) => {
+    try{
+        const errors = validationResult(req)
+        if (!errors.isEmpty()) {
+            return res.status(400).json({message: "Uncorrect request", errors})
+        }
+
+        const {email, password} = req.body; // получим имэил и пароль из тела запроса
+        const candidate = User.findOne({email}) // проверим существует ли пользователь с таким имэил в базе
+
+        if (candidate) {
+            return res.status(400).json({message: `User with email ${email} already exist`})
+        }
+        const hashPassword = await bcrypt.hash(password, 15) // хєшируем пароль для безопасности
+        const user = new User ({email, password: hashPassword})
+        await user.save() //сохраним нового поьзователя в БД
+        return res.json ({message: "User was created"})
+        
+    } catch (e) {
+        console.log(e)
+        res.send({message: "Server error"})
+    }
+})
+
+module.exports = router
