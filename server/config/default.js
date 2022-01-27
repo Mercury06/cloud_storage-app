0d1f7a46e03305d63e4a6bfb43ba39a3f94a98dc
Mercury06 <zetmail@ukr.net>
2022-01-27 21:56:11 +0300

    global variables added to default.js

--- server/config/default.js
+++ server/config/default.js
@@ -1,5 +1,6 @@
+module.exports = 
 {
-    "serverPort": 5060,
+    "serverPort": 3000,
     "dbUrl": "mongodb+srv://agregator:cloud_password93@cloud-disk.uvdnf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 }
 
--- server/index.js
+++ server/index.js
@@ -1,21 +1,24 @@
 const express = require ("express");
 const res = require("express/lib/response")
 const mongoose = require ("mongoose")
-//var config = require('config');
+const config = require('./config/default');
 const router = require ("./routes/auth.routes.js")
 
 const { db } = require ("./models/User.js");
 
-const app = express()
-PORT = 3000;
+const app = express();
+const PORT = config.serverPort;
+const dbUrl = config.dbUrl;
+//PORT = 3000;
 app.use(express.json())
 app.use("/api/auth", router)
 
 const start = async () => {
     try {
 
-    await mongoose.connect("mongodb+srv://agregator:cloud_password93@cloud-disk.uvdnf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
-       
+    //await mongoose.connect("mongodb+srv://agregator:cloud_password93@cloud-disk.uvdnf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+    await mongoose.connect(dbUrl)
+    
     app.get ("/", (req, res) => {
         res.send('Some text');
     })
