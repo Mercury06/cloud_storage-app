12696820ea94abbb0640a3a13f821df09ca3411e
Mercury06 <zetmail@ukr.net>
2022-01-28 11:28:35 +0300

    jwd token added

--- server/config/default.js
+++ server/config/default.js
@@ -1,6 +1,7 @@
 module.exports = 
 {
     "serverPort": 3000,
-    "dbUrl": "mongodb+srv://agregator:cloud_password93@cloud-disk.uvdnf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+    "dbUrl": "mongodb+srv://agregator:cloud_password93@cloud-disk.uvdnf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    "secretKey": "mern-secret-key"
 }
 
--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -1,9 +1,12 @@
 const Router = require ("express");
 const User = require ("../models/User")
 const bcrypt = require ("bcryptjs") 
+const config = require('../config/default');
+const jwt = require("jsonwebtoken")
 const {check, validationResult} =  require ('express-validator')
 
 const router = new Router()  //создаем объект
+const secretKey = config.secretKey;
 
 router.post('/reg', 
     [
@@ -11,7 +14,7 @@ router.post('/reg',
         check('password', 'Password must be longer than 3 and shorter than 12').isLength({min:3, max:12})
     ],    
     async (req, res) => {
-    console.log("from POST" + req.body)
+    console.log("from POST")
     //try{
         const errors = validationResult(req)
         if (!errors.isEmpty()) {
@@ -23,7 +26,7 @@ router.post('/reg',
         if (candidate) {
             return res.status(400).json({message: `User with email ${email} already exist`})
         }
-        const hashPassword = await bcrypt.hash(password, 15) // хєшируем пароль для безопасности
+        const hashPassword = await bcrypt.hash(password, 8) // хєшируем пароль для безопасности
         const user = new User ({email, password: hashPassword})
         await user.save() //сохраним нового поьзователя в БД
         return res.json ({message: "User was created"})
@@ -34,6 +37,34 @@ router.post('/reg',
     //     res.send({message: "Server error"})
     // }
 //})
+router.post('/login', 
+   
+    async (req, res) => {
+    console.log("from login")
+    const { email, password } = req.body;
+    const user = await User.findOne ({email})
+    
+    if (!user) {
+        return res.status(404).json({ message: "User not found"})
+    }
+
+    const isPassValid = bcrypt.compareSync(password, user.password)
+
+    if (!isPassValid) {
+        return res.status(400).json({message: "Password is incorrect"})
+    }
+    const token = jwt.sign({ id: user.id }, secretKey, { expiresIn: "1h" })
+    return res.json ({
+        token,
+        user: {
+            id: user.id,
+            email: user.email,
+            diskSpace: user.diskSpace,
+            usedSpace: user.usedSpace,
+            avatar: user.avatar
+        }
+    })
+})
 
 //export default router;
 module.exports = router
