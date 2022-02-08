33c7ddbf7a654337ebce51ed08422103c0fbda9c
Mercury06 <zetmail@ukr.net>
2022-02-08 10:46:28 +0300

    file size converted and css animation added

--- client/src/components/disk/fileList/file/File.jsx
+++ client/src/components/disk/fileList/file/File.jsx
@@ -5,6 +5,7 @@ import fileLogo from '../../../../assets/img/file.svg';
 import { useDispatch, useSelector } from 'react-redux';
 import { pushToStack, setCurrentDir } from '../../../../reducers/fileReducer';
 import { deleteFile, downloadFile } from '../../../../actions/file';
+import sizeFormat from "../../../../utils/sizeFormat";
 
 const File = ({file}) => {
 
@@ -34,7 +35,7 @@ const File = ({file}) => {
             <img src={file.type === 'dir' ? dirLogo : fileLogo} alt='' className='file_img' />
             <div className='file_name'>{file.name}</div>
             <div className='file_date'>{file.date.slice(0,10)}</div>
-            <div className='file_size'>{file.size}</div>
+            <div className='file_size'>{sizeFormat(file.size)}</div>
             { file.type !== 'dir' && <button onClick={(e) => downloadClickHandler(e)} className='file_btn file_download'>download</button>}
             <button onClick={(e) => deleteClickHandler(e)} className='file_btn file_delete'>delete</button>
             
--- client/src/components/disk/fileList/file/FileList.jsx
+++ client/src/components/disk/fileList/file/FileList.jsx
@@ -2,22 +2,30 @@ import React from "react";
 import './../fileList.css'
 import { useSelector } from "react-redux";
 import File from "./File";
+import {CSSTransition, TransitionGroup} from "react-transition-group";
 
 const FileList = () => {
 
-    const files = useSelector( state => state.files.files).map(file => <File file={file} key={file._id}/>)
+    const files = useSelector( state => state.files.files)
     // const files = [{_id:1, name: 'direc', type: 'dir', size: '5Gb', date: "15.07.2019"},
     //                {_id:2, name: 'direc2', type: 'file', size: '5Gb', date: "15.07.2019"}].map(file => <File file={file} key={file.id} />)
 
     return (
         <div className="filelist">
-            <div className="filelist_header">
-                <div className="filelist_name"> Название </div>
-                <div className="filelist_date"> Дата </div>
-                <div className="filelist_size"> Размер </div>
+            <div className="filelist__header">
+                <div className="filelist__name"> Название </div>
+                <div className="filelist__date"> Дата </div>
+                <div className="filelist__size"> Размер </div>
             </div>
-            {files}
+            <TransitionGroup>
+            {files.map(file => 
+            <CSSTransition  key={file._id} timeout={500} classNames={'file'} exit={false}>
+            <File file={file}/>
+            </CSSTransition>
+            )}
+            </TransitionGroup>
         </div>
+        
     );
 };
 
