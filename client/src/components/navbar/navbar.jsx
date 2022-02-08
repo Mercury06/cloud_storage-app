9c825a6154baa466d07220054779e145ad943de7
Mercury06 <zetmail@ukr.net>
2022-02-08 13:21:00 +0300

    files search mode added

--- client/src/components/navbar/navbar.jsx
+++ client/src/components/navbar/navbar.jsx
@@ -1,15 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import './navbar.css'
 //import Logo from './assets/img/navbar-logo.svg'
 import {NavLink} from "react-router-dom";
 import {logout} from "../../reducers/userReducer"
+import {showLoader} from "../../reducers/appReducer";
+import {getFiles, searchFiles} from "../../actions/file";
 
 
 const Navbar = () => {
 
     const isAuth = useSelector( state => state.user.isAuth )
+    const currentDir = useSelector( state => state.files.currentDir)
     const dispatch = useDispatch()
+    const [searchName, setSearchName] = useState('')
+    const [searchTimeout, setSearchTimeout] = useState(false)
+
+    function searchChangeHandler(e) {
+        setSearchName(e.target.value)
+        if (searchTimeout !== false) {
+            clearTimeout(searchTimeout)
+        }
+        dispatch(showLoader())
+        if(e.target.value !== '') {
+            setSearchTimeout(setTimeout((value) => {
+                dispatch(searchFiles(value));
+            }, 1000, e.target.value))
+        } else {
+            dispatch(getFiles(currentDir))
+        }
+    }
 
     return (
         <div className="navbar">
@@ -17,12 +37,18 @@ const Navbar = () => {
             <div className="container">
             
                 {/* <img src={Logo} alt="" classname="navbar_logo" /> */}
-                <div className="navbar_header">MERN CLOUD</div>               
-                
-                { !isAuth && <div className="navbar_login"><NavLink to="/login">Войти</NavLink></div>}                
-                { !isAuth && <div className="navbar_registration"><NavLink to="/registration">Регистрация</NavLink></div>}
+                <div className="navbar__header">MERN CLOUD</div>            
 
-                { isAuth && <div onClick={() => dispatch(logout())}>Выход</div>}
+                    { isAuth && 
+                        <input className="navbar__search" 
+                               type="text" 
+                               placeholder='enter file name...'
+                               value={searchName}
+                               onChange={e => searchChangeHandler(e)} />} 
+                    { !isAuth && <div className="navbar_login"><NavLink to="/login">Войти</NavLink></div>}                
+                    { !isAuth && <div className="navbar_registration"><NavLink to="/registration">Регистрация</NavLink></div>}
+
+                    { isAuth && <div onClick={() => dispatch(logout())}>Выход</div>}
             </div>            
         </div>
     );
